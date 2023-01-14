b61a571749d536a27fe1f241ee95871241155ea6
Patience-Fuglo <pfuglo@fordham.edu>
2023-01-14 14:58:13 -0500

    Done

--- public/index.js
+++ public/index.js
@@ -1,26 +1,44 @@
-async function main(){
-    const countContainer = document.querySelector('#count-container');
-    const incrementButton = document.querySelector('#increment-button');
-    const decrementButton = document.querySelector('#decrement-button');
-
-    const response = await fetch('http://localhost:9001/counter');
-
+async function main() {
+    const countContainer = document.querySelector("#count-container");
+    const incrementButton = document.querySelector("#increment-button");
+    const decrementButton = document.querySelector("#decrement-button");
+    const resetButton = document.querySelector("#reset-button");
+    const URL = "http://localhost:9001";
+    const response = await fetch("http://localhost:9001/counter");
+  
     const result = await response.json();
-    
+  
     let countValue = result.value;
-
-    function increment(){
-        countValue++;
-        countContainer.textContent = countValue;
+    function generateOption(value, method) {
+      return {
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ value }),
+      };
     }
-
-    function decrement(){
-        countValue--;
-        countContainer.textContent = countValue;
+  
+    function increment() {
+      countValue++;
+      fetch(`${URL}/counter`, generateOption(countValue, "PATCH"));
+      countContainer.textContent = countValue;
     }
-
-    incrementButton.addEventListener('click', increment);
-    decrementButton.addEventListener('click', decrement);
+  
+    function decrement() {
+      countValue--;
+      fetch(`${URL}/counter`, generateOption(countValue, "PATCH"));
+      countContainer.textContent = countValue;
+    }
+  
+    function reset() {
+      fetch(`${URL}/counter`, generateOption(0, "PATCH"));
+      main();
+    }
+  
+    incrementButton.addEventListener("click", increment);
+    decrementButton.addEventListener("click", decrement);
+    resetButton.addEventListener("click", reset);
     countContainer.textContent = countValue;
-}
-main()
+  }
+  main();
